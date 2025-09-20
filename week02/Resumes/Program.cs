ffe0862168bf535f5450f99122adc7d7ffb67a9b
uba41 <ubagabriel41@gmail.com>
2025-09-20 17:50:10 +0100

    Journal

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+
     }
 }
